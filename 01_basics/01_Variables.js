eb71f7baac22f06317cf5314246a095a6fe4e1f6
Prateek Kumar <138985323+prateekumar27@users.noreply.github.com>
2026-06-07 11:00:48 +0000

    Variables and Datatypes

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,26 @@
+// {
+//     let name = "monu"
+//     name = "kumar"
+//     console.log(name);  
+// }
+
+// function greet() {
+//     var a = "10"
+//     // console.log(a);   
+// }
+// console.log(a);
+// greet()
+
+if(true){
+    var a = "10"
+}
+console.log(a);
+
+
+
+
+
+
+
+// console.table([accountId, name, state]);
+
