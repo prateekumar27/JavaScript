e8651514aba9deadfda6e47709f02d634ed4a42c
Prateek Kumar <138985323+prateekumar27@users.noreply.github.com>
2026-06-19 07:07:56 +0000

    conversion of datatypes

---
+++ 01_basics/03_DataConversion.js
@@ -0,0 +1,13 @@
+// let score = "33"
+// let number = Number(score)
+// console.log(number);
+// console.log(typeof(number));
+
+
+let score = "333abc"
+let number = Number(score)
+console.log(number);
+console.log(typeof(number));
+
+
+
