8910f9990f69e8e833920df14019773863e20e64
Prateek Kumar <138985323+prateekumar27@users.noreply.github.com>
2026-06-19 08:34:01 +0000

    prefix and postfix

--- 01_basics/03_DataConversion.js
+++ 01_basics/03_DataConversion.js
@@ -4,10 +4,40 @@
 // console.log(typeof(number));
 
 
-let score = "333abc"
-let number = Number(score)
-console.log(number);
-console.log(typeof(number));
+// let score = "333abc"
+// let number = Number(score)
+// console.log(number);
+// console.log(typeof(number));
+
+
+
+
+/////////////////////////////////operations/////////////////////////////
+
+
+// console.log("2" + 1); //string concetinate
+// console.log("2" - 1); // number conversion
+
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+//postfix
+
+let x = 5;
+console.log(x++); 
+console.log(x);
+
+let y = 5
+console.log(++y);
+
+
+
+
+
+
 
 
 
