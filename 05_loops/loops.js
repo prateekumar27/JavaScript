58c2dfea29c635ca68e18e2f7741067d93ab3605
Prateek Kumar <138985323+prateekumar27@users.noreply.github.com>
2026-07-03 09:30:50 +0000

    loops

---
+++ 05_loops/loops.js
@@ -0,0 +1,18 @@
+//iteration
+
+//for loop
+// for (let i = 0; i <= 10; i++) {
+// console.log(i);
+// }
+
+// for (let i = 1; i <= 10 ; i++) {
+//     //console.log(`Outer loop ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`Inner loop ${j}, inner loop ${i}`) 
+//         //console.log(i + "*" + j + "=" + i * j);  
+//     }  
+// }
+
+
+
+
