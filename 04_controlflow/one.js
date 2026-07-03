10e6006d9b9b2f498cd80ef425477f558476eaaf
Prateek Kumar <138985323+prateekumar27@users.noreply.github.com>
2026-07-03 08:35:55 +0000

    commit

--- 04_controlflow/one.js
+++ 04_controlflow/one.js
@@ -42,22 +42,96 @@
 // }
 
 
-const userLoggedIn = true
-const debitCard = true
-const loggedInFromGoogle = false
-const loggedInFromEmail = true
 
-if (userLoggedIn && debitCard) {
-    console.log("alllow to buy course");
-    
-}
+//OR , AND , ??(nullish coalescing operator),
+//TERNIARY ?
 
-if (loggedInFromGoogle || loggedInFromEmail) {
-    console.log("user logged in");
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80");
+;
+
+
+//to provide a default value when a varibale is null and undefineed
+let username = null
+console.log(username ?? "guest")
+
+let val1;
+val1 = 5 ?? 10
+val1 = null ?? 10
+val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+
+
+
+// const userLoggedIn = true
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if (userLoggedIn && debitCard) {
+//     console.log("alllow to buy course");
     
-}
+// }
+
+// if (loggedInFromGoogle || loggedInFromEmail) {
+//     console.log("user logged in");
+    
+// }
 
 //switch statement
+// const month = 3
 
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+//     case 5:
+//         console.log("may");
+//         break;
+
+//     default:
+//         console.log("default match");
+//         break;
+// }
+
+
+//truethy // falsy
+
+// const userEmail = ""
+
+// if (userEmail) {
+//     console.log("got user email");
+    
+// }else{
+//     console.log("dont have user email");
+    
+// }
+
+//falsy value = false , "" , 0 , -0 , bigInt 0n , null, undefined , Nan
+
+//truthy value = "0" , "false" , "space" , [] , {} , function(){} , 
+
+//when we assume its a true value 
+
+const array = []
+if (array.length === 0) {
+    console.log("array is empty");
+    
+}
+const object = {}
+if (Object.keys(object).length === 0) {
+    console.log("object is empty");
+    
+}
 
 
