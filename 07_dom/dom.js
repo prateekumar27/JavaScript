41d7477c340de53e687887b7fc9c438c3414c3a0
Prateek Kumar <138985323+prateekumar27@users.noreply.github.com>
2026-08-10 10:14:02 +0000

    dom

--- 07_dom/dom.js
+++ 07_dom/dom.js
@@ -4,19 +4,23 @@
 //console.log(document)
 
 
+//This allows JavaScript to access and modify the content, structure, and styles of a web page dynamically.
+//that represents an HTML
+
+
 //++++++<h1 id="title" class="heading">Dom </h1>
 
-//document.getElementById("title") = 
+//document.getElementById("title") = <h1 id="title" class="heading">Dom </h1>
 
-//document.getElementById("title").id
+//document.getElementById("title").id = "title"
 
-//document.getElementById("title").className
+//document.getElementById("title").className = "heading"
 
 //document.getElementById("title").getAttribute("class") = "heading"
 
 //document.getElementById("title").getAttribute("id") = "title"
 
-//document.getElementById("title").setAttribute("class", "test") = always override now class = heading
+//document.getElementById("title").setAttribute("class", "test") = always override now class=test on concole but on document class = heading
 
 //++++++++Styling+++++++++//
 //const heading = document.getElementById("title");
@@ -57,24 +61,24 @@
 
 // const myul = document.querySelector("ul")
 
-// myul.querySelector("li")
+// myul.querySelector("li") = always selct first elemet
 
 // const turngreen = myul.querySelector("li")
 
 // turngreen.style.background = "green"
 
 //turngree.innnertext //one
-//turngree.innnertext = "five"
+//turngree.innnertext = "five" //one will become five
 
 //what datatype they are returning = diffrence
 
 
 //+++++  QUERY SELCTOR ALL ++++++// prototype
 
-//document.querySelectorAll("li") = we will get a nodelist and prototype 
+//document.querySelectorAll("li") = we will get a nodelist and prototype(forech)
 
 //const list = document.querySelectorAll("li")
-//list[0].style.color = "red" //use[0] this important
+//list[0].style.color = "red" //use[0] //this important
 
 // list.foreach ( (li) => {
 // li.style.color = "green"
@@ -91,6 +95,7 @@
 </ul> */}
 
 // document.getElementsByClassName("list") = we will get HTML COLLECTION but we have to convert this into an array first to get prototype
+//node collection = prototype and html collection(no prototype) are diff
 
 //const tempclasslist =  document.getElementsByClassName("list") 
 
@@ -99,6 +104,95 @@
 // converted.foreach( (li) => {
 //   li.style.color = "blue"
 //   })
+
+
+
+
+
+
+
+//dom selector = methods to find html element using javascript
+
+//class="heading"
+//class="heading"
+//getelemntbyclassname("heading")
+//it will return html collection
+
+
+//query selector
+//Uses CSS selectors and returns the first matching element.
+
+// document.querySelector(".heading")
+// document.querySelector("#title")
+// document.querySelector("p")
+
+// If there are 5 <p> elements, it returns only the first <p>.
+
+
+// What is NodeList?
+
+// A NodeList is a collection of nodes returned by some DOM methods.
+
+// For example:
+
+// const items = document.querySelectorAll("li");
+
+// console.log(items);
+
+// You might get:
+
+// NodeList(3) [li, li, li]
+
+// You can access elements using indexes:
+
+// console.log(items[0]);
+// console.log(items[1]);
+
+// You can also use forEach():
+
+// items.forEach((item) => {
+//     console.log(item);
+// });
+
+
+
+
+// What is HTMLCollection?
+
+// An HTMLCollection is a collection of HTML elements.
+
+// For example:
+
+// const headings = document.getElementsByClassName("heading");
+
+// console.log(headings);
+
+// You might get:
+
+// HTMLCollection(3) [h1.heading, h2.heading, h3.heading]
+
+// You can access them using indexes:
+
+// console.log(headings[0]);
+// console.log(headings[1]);
+
+// But unlike a NodeList, an HTMLCollection does not have forEach() directly.
+
+// So this won't work:
+
+// headings.forEach(...)
+
+// Instead, you can use a normal for loop:
+
+// for (let i = 0; i < headings.length; i++) {
+//     console.log(headings[i]);
+// }
+
+// Or convert it to an array:
+
+// Array.from(headings).forEach((item) => {
+//     console.log(item);
+// });
 
 
 
